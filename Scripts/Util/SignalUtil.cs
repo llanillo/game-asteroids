43977b28da5f531b91abfd8c6ad852c95d7644f1
Octavio Llanillo <llanillo4@hotmail.com>
2022-08-09 21:22:16 -0300

    Create signal util class

---
+++ Scripts/Util/SignalUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace Asteroids.Util;
+
+public class SignalUtil
+{
+    public const string Timeout = "timeout";
+    public const string ScreenExited = "screen_exited";
+    public const string Pressed = "pressed";
+    public const string TweenStarted = "tween_started";
+    public const string TweenCompleted = "tween_completed";
+    public const string BodyEntered = "body_entered";
+}
